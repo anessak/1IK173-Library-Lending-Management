c5ca371249ce226c7f5fa83841183e1501dd2eb0
anessa <anessa@hotmail.se>
2020-05-15 16:47:04 +0200

    Lägger ny tom testklass

---
+++ MembershipAdministration/tests/com/llm/membershipadmin/MembershipManagerTest.java
@@ -0,0 +1,23 @@
+package com.llm.membershipadmin;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MembershipManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registerNewLibraryMember_Test() {
+        assertEquals(1,1);
+    }
+}
