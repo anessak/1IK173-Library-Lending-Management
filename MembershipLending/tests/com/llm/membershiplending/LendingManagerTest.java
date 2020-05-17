14425e7c31a4726bb73d80105092884584779437
anessa <anessa@hotmail.se>
2020-05-17 02:09:05 +0200

    FIxat testarna i Lending Modul

--- MembershipLending/tests/com/llm/membershiplending/LendingManagerTest.java
+++ MembershipLending/tests/com/llm/membershiplending/LendingManagerTest.java
@@ -16,10 +16,11 @@ class LendingManagerTest {
     void lendBookItems_WhenMemeberHasNoLendings() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        var db=mock(IMemberLendingStore.class);
+        //var db = mock(MemberLendingStore.class,withSettings()
+          //      .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
+
 
         var member=new Member(1000,0,0,6);
         ArrayList<LendingBasketEntity> lbe= new ArrayList<>();
@@ -28,7 +29,6 @@ class LendingManagerTest {
         lbe.add(new LendingBasketEntity(200, UUID.randomUUID(),
                 LocalDateTime.of(2019, Month.OCTOBER, 20, 6, 30)));
         var ml=new MemberLending(100,lbe);
-
         when(db.getMember(member.getMemberId())).thenReturn(member);
         doNothing().when(db).addNewLendingBasketForMember(ml);
         when(db.getMemberBorrowedBookItems(member.getMemberId())).thenReturn(ml);
@@ -47,8 +47,7 @@ class LendingManagerTest {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
 
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
 
         var member=new Member(1000,0,0,1);
@@ -61,15 +60,14 @@ class LendingManagerTest {
 
         when(db.getMember(ml.getMemberId())).thenReturn(null);
 
-        assertEquals("No Member Was found",mgr.lendBookItems(ml) );
+        assertEquals(LendingResultMessage.Error,mgr.lendBookItems(ml) );
     }
     @Test
     void lendBookItems_WhenMemberTriesToBorrowToManyBooksReturnError() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
 
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
 
         var member=new Member(1000,0,0,1);
@@ -84,21 +82,19 @@ class LendingManagerTest {
         bookItemSomManHarredanUtlånat.add(new LendingBasketEntity(200, UUID.randomUUID(),
                 LocalDateTime.of(2019, Month.OCTOBER, 10, 6, 30)));
 
-        when(db.getMember(member.getMemberId())).thenReturn(member);
+        when(db.getMember(ml.getMemberId())).thenReturn(member);
         doNothing().when(db).addNewLendingBasketForMember(ml);
-        when(db.getMemberBorrowedBookItems(member.getMemberId())).thenReturn(new MemberLending(100,bookItemSomManHarredanUtlånat));
+        when(db.getMemberBorrowedBookItems(100)).thenReturn(new MemberLending(100,bookItemSomManHarredanUtlånat));
 
 
-        assertEquals("du har försökt låna för många böcker ta bort 2 stycken",mgr.lendBookItems(ml) );
+        assertEquals(LendingResultMessage.Conflict,mgr.lendBookItems(ml) );
     }
     @Test
     public void getMemberAndCurrentBorrowedBookItems_Correct() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
         ArrayList<LendingBasketEntity> lånadeBöcker= new ArrayList<>();
@@ -117,10 +113,8 @@ class LendingManagerTest {
     public void getMemberAndCurrentBorrowedBookItems_WithNoBorrowedBooks() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
 
@@ -133,27 +127,23 @@ class LendingManagerTest {
     public void getMemberAndCurrentBorrowedBookItem_WithNoMemberShouldReturnError() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
 
         when(db.getMember(member.getMemberId())).thenReturn(null);
         when(db.getMemberBorrowedBookItems(member.getMemberId())).thenReturn(null);
 
-        assertEquals(null,mgr.getMemberAndCurrentBorrowedBookItems(member.getMemberId()) );
+        assertNull(mgr.getMemberAndCurrentBorrowedBookItems(member.getMemberId()) );
 
     }
     @Test
     public void searchMemberBorrowedItems_Correct() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
         ArrayList<LendingBasketEntity> lånadeBöcker= new ArrayList<>();
@@ -172,26 +162,22 @@ class LendingManagerTest {
     public void searchMemberBorrowedItems_NoMemberFound() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
 
         when(db.getMember(member.getMemberId())).thenReturn(null);
 
-        assertEquals(null,mgr.searchMemberBorrowedItems(member.getMemberId()) );
+        assertNull(mgr.searchMemberBorrowedItems(member.getMemberId()) );
 
     }
     @Test
     public void returnBorrowedItem_Correct() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
         ArrayList<UUID> returnBöckerIDs= new ArrayList<>();
@@ -200,19 +186,17 @@ class LendingManagerTest {
         var rl=new ReturnLendBasket(100,returnBöckerIDs,
                 LocalDateTime.of(2019, Month.OCTOBER, 10, 6, 30));
 
-        when(db.getMember(member.getMemberId())).thenReturn(member);
+        when(db.getMember(rl.getMemberId())).thenReturn(member);
         when(db.removeBookItemFromLending(rl)).thenReturn(0);
 
-        assertEquals(0,mgr.returnBorrowedItem(rl) );
+        assertEquals(LendingResultMessage.Ok,mgr.returnBorrowedItem(rl) );
     }
     @Test
     public void returnBorrowedItem_NoMemberFoundShouldReturnError() {
         var logger= mock(Logger.class);
         var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         ArrayList<UUID> returnBöckerIDs= new ArrayList<>();
         var uuid= UUID.randomUUID();
@@ -222,17 +206,15 @@ class LendingManagerTest {
 
         when(db.getMember(100)).thenReturn(null);
 
-        assertEquals(-1,mgr.returnBorrowedItem(rl) );
+        assertEquals(LendingResultMessage.Error,mgr.returnBorrowedItem(rl) );
 
     }
     @Test
     public void returnBorrowedItem_DataBaseRemovalReturnsError() {
         var logger= mock(Logger.class);
-        var bus=EventBus.getDefault();
-        IMemberLendingStore db = mock(MemberLendingStore.class,withSettings()
-                .useConstructor(logger).defaultAnswer(Answers.RETURNS_MOCKS));
+        var bus=mock(EventBus.class);
+        IMemberLendingStore db = mock(MemberLendingStore.class);
         LendingManager mgr= new LendingManager(db,logger,bus);
-        LendingScheduler.Init(db,logger,bus);
 
         var member=new Member(1000,0,0,6);
         ArrayList<UUID> returnBöckerIDs= new ArrayList<>();
@@ -244,6 +226,6 @@ class LendingManagerTest {
         when(db.getMember(member.getMemberId())).thenReturn(member);
         when(db.removeBookItemFromLending(rl)).thenReturn(-1);
 
-        assertEquals(-1,mgr.returnBorrowedItem(rl) );
+        assertEquals(LendingResultMessage.Error,mgr.returnBorrowedItem(rl) );
     }
 }
