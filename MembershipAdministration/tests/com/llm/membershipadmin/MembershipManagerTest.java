0567e91921ab56c6ff4e74ce4ef50e2484da94d0
anessa <anessa@hotmail.se>
2020-05-17 03:22:09 +0200

    Första testerna i MemberShipAdmin modulen

--- MembershipAdministration/tests/com/llm/membershipadmin/MembershipManagerTest.java
+++ MembershipAdministration/tests/com/llm/membershipadmin/MembershipManagerTest.java
@@ -7,9 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.withSettings;
+import static org.mockito.Mockito.*;
 
 class MembershipManagerTest {
 
@@ -23,7 +24,83 @@ class MembershipManagerTest {
 
     @Test
     void registerNewLibraryMember_Test() {
+        var logger= mock(Logger.class);
+        var bus= EventBus.getDefault();
+        var db=mock(IMembershipStore.class);
+        var mgr= new MembershipManager(db,logger,bus);
+        //Create member
+        var nowDate= LocalDateTime.now();
+        var member1=new Member(1000,"ssn",
+                "fname","ename", MemberRole.Undergraduate,
+                MemberStatus.Active,"pwd", nowDate);
+        var member2=new Member(2000,"ssn",
+                "fname","ename", MemberRole.PhD,
+                MemberStatus.Active,"pwd", nowDate);
 
-        assertEquals(1,1);
+        when(mgr.getMemberById(member1.getMemberId())).thenReturn(member1);
+        when(db.insertNewMember(member2)).thenReturn(0);
+
+        assertEquals(MemberShipResultMessage.Ok, mgr.registerNewLibraryMember(member2));
+    }
+    @Test
+    void registerNewLibraryMember_DUplicateMemberReturnConflict() {
+        var logger= mock(Logger.class);
+        var bus= EventBus.getDefault();
+        var db=mock(IMembershipStore.class);
+        var mgr= new MembershipManager(db,logger,bus);
+        //Create member
+        var nowDate= LocalDateTime.now();
+        var member1=new Member(1000,"ssn",
+                "fname","ename", MemberRole.Undergraduate,
+                MemberStatus.Active,"pwd", nowDate);
+        var member2=new Member(2000,"ssn",
+                "fname","ename", MemberRole.PhD,
+                MemberStatus.Active,"pwd", nowDate);
+
+        when(mgr.getMemberById(member1.getMemberId())).thenReturn(member1);
+        when(db.insertNewMember(member1)).thenReturn(-1);
+
+        assertEquals(MemberShipResultMessage.Conflict, mgr.registerNewLibraryMember(member1));
+
+    }
+    @Test
+    void registerNewLibraryMember_SuspendedMemberReturnSuspend() {
+        var logger= mock(Logger.class);
+        var bus= EventBus.getDefault();
+        var db=mock(IMembershipStore.class);
+        var mgr= new MembershipManager(db,logger,bus);
+        //Create member
+        var nowDate= LocalDateTime.now();
+        var member1=new Member(1000,"ssn",
+                "fname","ename", MemberRole.Undergraduate,
+                MemberStatus.Suspended,"pwd", nowDate);
+
+        when(mgr.getMemberById(member1.getMemberId())).thenReturn(member1);
+        when(db.insertNewMember(member1)).thenReturn(0);
+
+        assertEquals(MemberShipResultMessage.Suspended, mgr.registerNewLibraryMember(member1));
+
+    }
+    @Test
+    void registerNewLibraryMember_SuspendedMemberReturnError() {
+        var logger= mock(Logger.class);
+        var bus= EventBus.getDefault();
+        var db=mock(IMembershipStore.class);
+        var mgr= new MembershipManager(db,logger,bus);
+        //Create member
+        var nowDate= LocalDateTime.now();
+        var member1=new Member(1000,"ssn",
+                "fname","ename", MemberRole.Undergraduate,
+                MemberStatus.Active,"pwd", nowDate);
+
+        var member2=new Member(2000,"ssn",
+                "fname","ename", MemberRole.Postgraduate,
+                MemberStatus.Active,"pwd", nowDate);
+
+        when(mgr.getMemberById(member1.getMemberId())).thenReturn(member1);
+        when(db.insertNewMember(member2)).thenReturn(-1);
+
+        assertEquals(MemberShipResultMessage.Error, mgr.registerNewLibraryMember(member2));
+
     }
 }
